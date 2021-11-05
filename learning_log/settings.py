9e55bdafbaa4faf1516f352869dabb4dc1be2661
ilya-galkin <iljagalkin@ro.ru>
2021-11-05 14:54:56 +0300

    Settings changed

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from .secrets import *
+
+
 from pathlib import Path
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_r13rl%dzk1*zkgjt8^joi$xeai499w%-j0a3@v%n=dz-i*ut9'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
